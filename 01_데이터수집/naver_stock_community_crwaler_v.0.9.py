f63bec7bb24e45f5c1ded31c431075f4c55086d8
Ricky Choi <stagvii@gmail.com>
2025-06-27 13:47:43 +0900

    크롤러 0.9버전 코드 명세서 추가

--- 01_데이터수집/naver_stock_community_crwaler_v.0.9.py
+++ 01_데이터수집/naver_stock_community_crwaler_v.0.9.py
@@ -3,7 +3,6 @@ import re
 import random
 import time
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
